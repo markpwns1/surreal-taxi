e6aed0280906a046a692e1872f11ff287d7237ca
nashdashin <srivastavanikhil226@yahoo.ca>
2021-08-28 11:53:07 -0600

    changes

--- Assets/Scripts/otherScript.cs
+++ Assets/Scripts/otherScript.cs
@@ -1,18 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class otherScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public GameObject scoreText;
+    public int theScore;
+
+    void OnTriggerEnter(Collider other) {
+
+        if (other.gameObject.name == "car")
+        {
+            theScore = 1000;
+            scoreText.GetComponent<Text>().text = "Salary: " + theScore+ "$";
+            Destroy(gameObject);
+
+        }
+        //Time.deltaTime //variable
+
+        //
+
+      
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+
 }
