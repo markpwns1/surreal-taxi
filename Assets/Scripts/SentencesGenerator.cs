cc5c873dd7c5ebc169de2f338029ef3b2e7452f6
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2021-08-27 18:51:29 -0600

    Update SentencesGenerator.cs

--- Assets/Scripts/SentencesGenerator.cs
+++ Assets/Scripts/SentencesGenerator.cs
@@ -9,20 +9,16 @@ public class SentencesGenerator : MonoBehaviour
 {
     public List<SentencesSource> references;
     public StringMarkov model;
-    // Start is called before the first frame update
-    void Start()
-    {
-        // Create a new model
-        model = new StringMarkov(2);
-
-        foreach(SentencesSource source in references)
-        {
-            model.Learn(source.source);
-        }
-    }
 
     public string GenerateLines()
     {
+        model = new StringMarkov(2);
+
+        foreach (SentencesSource source in references)
+        {
+            model.Learn(source.source);
+        }
+
         return model.Walk().First();
     }
 }
