e26427d85e21938009b6376d0bd339be63f80830
SADragonMaker <44631569+SADragonMaker@users.noreply.github.com>
2021-08-27 18:54:06 -0600

    fix null pointer error

--- Assets/Scripts/SentencesGenerator.cs
+++ Assets/Scripts/SentencesGenerator.cs
@@ -8,11 +8,10 @@ using System.Linq;
 public class SentencesGenerator : MonoBehaviour
 {
     public List<SentencesSource> references;
-    public StringMarkov model;
 
     public string GenerateLines()
     {
-        model = new StringMarkov(2);
+        var model = new StringMarkov(2);
 
         foreach (SentencesSource source in references)
         {
