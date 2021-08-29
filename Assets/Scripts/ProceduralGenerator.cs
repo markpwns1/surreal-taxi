704f8523a9581c91a889301ea3e8ef752f9655f3
Mark <mark_kaldas@yahoo.com>
2021-08-28 21:06:31 -0600

    idk

--- Assets/Scripts/ProceduralGenerator.cs
+++ Assets/Scripts/ProceduralGenerator.cs
@@ -21,30 +21,74 @@ public class ProceduralGenerator : MonoBehaviour
     public Color[] colours;
     public float mapSize;
 
+    public GameObject player;
+
+    public struct ObjectPlacement
+    {
+        public bool success;
+        public GameObject instance;
+        public RaycastHit hit;
+    }
+
+    public ObjectPlacement PlaceObject(GameObject prefab, bool onFloor, bool repeat)
+    {
+        Vector3 origin = new Vector3(Random.Range(-mapSize / 2f, mapSize / 2f), 500, Random.Range(-mapSize / 2f, mapSize / 2f));
+
+        var j = 0;
+        do
+        {
+            if (Physics.Raycast(origin, Vector3.down, out RaycastHit hit) && (!onFloor || hit.collider.gameObject.tag == "Floor"))
+            {
+                var instance = Instantiate(prefab);
+
+                return new ObjectPlacement()
+                {
+                    success = true,
+                    hit = hit,
+                    instance = instance
+                };
+            }
+
+            j++;
+        } while (repeat && j < 100);
+
+        return new ObjectPlacement()
+        {
+            success = false
+        };
+    }
+
     void Start()
     {
         foreach (var obj in mapObjects)
         {
             for (int i = 0; i < obj.amount; i++)
             {
-                Vector3 origin = new Vector3(Random.Range(-mapSize / 2f, mapSize / 2f), 500, Random.Range(-mapSize / 2f, mapSize / 2f));
+                var placement = PlaceObject(obj.prefab, true, obj.repeat);
 
-                var valid = false;
-                var j = 0;
-                do {
-                    if (Physics.Raycast(origin, Vector3.down, out RaycastHit hit) && hit.collider.gameObject.tag == "Floor")
-                    {
-                        var instance = Instantiate(obj.prefab);
-                        instance.transform.position = hit.point + Vector3.up * Random.Range(obj.minHeight, obj.maxHeight);
-                        instance.transform.Rotate(new Vector3(Random.Range(0f, obj.maxAngle), Random.Range(0f, 360f), Random.Range(0f, obj.maxAngle)));
-                        instance.transform.localScale = Vector3.one * Random.Range(obj.minSize, obj.maxSize);
-                        valid = true;
-                        instance.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_DarkColour", colours[Random.Range(0, colours.Length)]);
-                    }
+                if (!placement.success) continue;
+                var instance = placement.instance;
+                var hit = placement.hit;
 
-                    j++;
-                } while (obj.repeat && !valid && j < 100);
+                instance.transform.position = hit.point + Vector3.up * Random.Range(obj.minHeight, obj.maxHeight);
+                instance.transform.Rotate(new Vector3(Random.Range(0f, obj.maxAngle), Random.Range(0f, 360f), Random.Range(0f, obj.maxAngle)));
+                instance.transform.localScale = Vector3.one * Random.Range(obj.minSize, obj.maxSize);
+                instance.transform.GetChild(0).GetComponent<Renderer>().material.SetColor("_DarkColour", colours[Random.Range(0, colours.Length)]);
             }
         }
+
+        {
+            ObjectPlacement placement;
+
+            do
+            {
+                placement = PlaceObject(player, true, true);
+            } 
+            while (!placement.success);
+
+            placement.instance.transform.position = placement.hit.point + Vector3.up * 3f;
+        }
+        
     }
+
 }
