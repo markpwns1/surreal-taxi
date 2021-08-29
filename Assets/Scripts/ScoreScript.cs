3d19d19fdeb78241b5e5aca5212c9c276d3aaec5
nashdashin <srivastavanikhil226@yahoo.ca>
2021-08-29 03:34:32 -0600

    timescore

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -9,12 +9,15 @@ public class ScoreScript : MonoBehaviour
     // Start is called before the first frame update
 
     public GameObject scoreText;
-    public static int theScore;
+    public static float theScore;
+   
 
     void Update() {
 
+
         scoreText.GetComponent<Text>().text = "Salary: " + theScore + "$";
-    
+        theScore -= Time.deltaTime;
+
     }
 
 
